aab980770d98dd5902a99f295e4629398ef1a0d1
Armine Gyulazyan <armine.gyulazyan@gmail.com>
2023-05-30 23:36:30 +0400

    Remove files from master branch

--- problem_1.py
+++
@@ -1,9 +0,0 @@
-while True:
-	weight_KG = input("Please enter your weight in kilograms: ")
-		
-	if weight_KG.replace('.', '', 1).isdigit():
-		weight_P = float(weight_KG)*2.2
-		print("Weight in pounds:", round(weight_P,2))
-		break
-	
-
--- problem_2.py
+++
@@ -1,7 +0,0 @@
-import random
-
-count = 0
-while count < 50:
-	rand_num = random.randint(3,6)
-	count+=1
-	print("Random integer"+str(count)+" between 3 and 6 is = ", rand_num)
